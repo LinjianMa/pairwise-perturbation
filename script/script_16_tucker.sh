5526ecc4c1f4cb76a5c5d8a914e7e768c198e52c
LinjianMa <malinjianev@gmail.com>
2018-11-14 19:35:29 -0800

    add tucker scripts

---
+++ script/script_16_tucker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#----------------------------------------------------
+#SBATCH -J ctf_pp
+#SBATCH -o pp.bench.N16.n128.1e-2.o%j.out
+#SBATCH -e pp.bench.N16.n128.1e-2.o%j.err
+#SBATCH -p normal
+#SBATCH -N 16
+#SBATCH -n 128
+#SBATCH -t 04:00:00
+#SBATCH --mail-user=solomon2@illinois.edu
+#SBATCH --mail-type=all    # Send email at begin and end of job
+
+module list
+pwd
+date
+
+export CTF_PPN=8
+export OMP_NUM_THREADS=6
+
+ibrun ./test_ALS -model Tucker -tensor r2 -pp 0 -dim 6 -size 47 -rank 3 -maxiter 250 -filename Tucker_r2_nodes=1_pp=0_dim=6_size=47_rank=3.csv -resprint 10 -tol 1e-8
+ibrun ./test_ALS -model Tucker -tensor r2 -pp 1 -dim 6 -size 47 -rank 3 -maxiter 250 -filename Tucker_r2_nodes=1_pp=1_dim=6_size=47_rank=3_restol=1e-2.csv -pp_res_tol 1e-2 -resprint 10 -tol 1e-8
+ibrun ./test_ALS -model Tucker -tensor r2 -pp 1 -dim 6 -size 47 -rank 3 -maxiter 250 -filename Tucker_r2_nodes=1_pp=1_dim=6_size=47_rank=3_restol=5e-2.csv -pp_res_tol 5e-2 -resprint 10 -tol 1e-8
+
+
